294179cba072906b160faf2bc7dd7fe78ed8eeaf
yanliangliang <yanliangfdu@gmail.com>
2022-12-16 15:18:05 +0800

    something new

---
+++ main_lstm.py
@@ -0,0 +1,85 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import matplotlib.pyplot as plt
+from model.lstm import LstmRNN
+from src.args import parse_args
+from src.utils import set_random_seeds,summary
+from src.data import load_data,data_split
+from torch.autograd import Variable
+
+args = parse_args()
+
+
+# checking if GPU is available
+device = torch.device("cpu")
+if (torch.cuda.is_available()):
+    device = torch.device("cuda:0")
+    print('Training on GPU.')
+else:
+    print('No GPU available, training on CPU.')
+
+
+
+def main():
+    test_loss_list =[]
+    for fold in range(args.repetitions):
+        set_random_seeds(fold)
+        data_x,data_y = load_data()
+        train_x, train_y, test_x, test_y = data_split(data_x,data_y,args.trainsize)
+        train_x, train_y, test_x, test_y = train_x.to(device), train_y.to(device), test_x.to(device), test_y.to(device)
+        input_size_num = data_x.shape[1]
+        output_size_num = 1
+        lstm_model = LstmRNN(input_size=input_size_num, hidden_size = args.hidden, output_size= output_size_num , num_layers= args.layers).to(device)
+        print('LSTM model:', lstm_model)
+        print('model.parameters:', lstm_model.parameters)
+        print('train x tensor dimension:', Variable(train_x).size())
+
+        # optimizer
+        criterion = nn.MSELoss()
+        optimizer = torch.optim.Adam(lstm_model.parameters(), lr=args.lr)
+
+        prev_loss = 1000
+
+        for epoch in range(args.epochs):
+            output = lstm_model(train_x).to(device)
+            loss = criterion(output, train_y)
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            if loss < prev_loss:
+                torch.save(lstm_model.state_dict(), 'lstm_model.pt')  # save model parameters to files
+                prev_loss = loss
+
+            if loss.item() < 1e-4:
+                print('Epoch [{}/{}], Loss: {:.5f}'.format(epoch + 1, args.epochs, loss.item()))
+                print("The loss value is reached")
+                break
+            elif (epoch + 1) % 100 == 0:
+                print('Epoch: [{}/{}], Loss:{:.5f}'.format(epoch + 1, args.epochs, loss.item()))
+
+
+        # ----------------- test -------------------
+
+        lstm_model = lstm_model.eval()  # switch to testing model
+
+        # prediction on test dataset
+
+
+        output_test = lstm_model(test_x).to(device)
+        test_loss = criterion(output_test,test_y )
+        # print("test loss：", test_loss.item())
+        test_loss_list.append(test_loss.item())
+
+    log = summary(test_loss_list,args.repetitions)
+    print(log)
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
---
+++ model/lstm.py
@@ -0,0 +1,38 @@
+import numpy
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import matplotlib.pyplot as plt
+
+
+# Define LSTM Neural Networks
+class LstmRNN(nn.Module):
+    """
+        Parameters：
+        - input_size: feature size
+        - hidden_size: number of hidden units
+        - output_size: number of output
+        - num_layers: layers of LSTM to stack
+    """
+
+    def __init__(self, input_size, hidden_size=1, output_size=1, num_layers=1):
+        super().__init__()
+
+        self.lstm = nn.LSTM(input_size, hidden_size, num_layers)  # utilize the LSTM model in torch.nn
+        self.linear1 = nn.Linear(hidden_size, output_size)  # 全连接层
+
+    def forward(self, _x):
+        x, _ = self.lstm(_x)  # _x is input, size (seq_len, batch, input_size)
+        s,  h = x.shape  # x is output, size (seq_len, batch, hidden_size)
+        x = x.view(s , h)
+        x = self.linear1(x)
+        x = x.view(s , -1)
+        return x
+
+
+
+
+
+
+
+
