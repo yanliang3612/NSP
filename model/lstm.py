881f30f16806ba2231b56c5aea0703d666dcd0ea
Liang Yan <100582544+yanliang3612@users.noreply.github.com>
2022-12-20 07:06:24 +0800

    Update lstm.py

--- model/lstm.py
+++ model/lstm.py
@@ -18,7 +18,7 @@ class LstmRNN(nn.Module):
     def __init__(self, input_size, hidden_size=1, output_size=1, num_layers=1):
         super().__init__()
 
-        self.lstm = nn.LSTM(input_size, hidden_size, num_layers)  # utilize the LSTM model in torch.nn
+        self.lstm = nn.LSTM(input_size, hidden_size, num_layers)  
         self.linear1 = nn.Linear(hidden_size, output_size)  
 
     def forward(self, _x):
